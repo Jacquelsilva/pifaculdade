605afe2db6fc43e0f78c5796de3a3dbe214d57b9
Cvecc <claravecchios@gmail.com>
2025-06-25 15:20:26 -0300

    add card nos recursos

--- resources/views/recursos.blade.php
+++ resources/views/recursos.blade.php
@@ -17,11 +17,13 @@
     <div class="texto hero-text"> <!-- coloque a classe texto junto com hero-text -->
       <h1>Recursos</h1>
       <p>Conheça as principais funcionalidades e benefícios que o nosso produto oferece para você ou sua empresa.</p>
-      <ul>
-        <li>Funcionalidade A - Automatização de processos</li>
-        <li>Funcionalidade B - Relatórios personalizados</li>
-        <li>Funcionalidade C - Integração com plataformas populares</li>
-      </ul>
+      <div class="card-recursos">
+        <ul>
+          <li>Funcionalidade A - Automatização de processos</li>
+          <li>Funcionalidade B - Relatórios personalizados</li>
+          <li>Funcionalidade C - Integração com plataformas populares</li>
+        </ul>
+      </div>
     </div>
 </section>
 @endsection
